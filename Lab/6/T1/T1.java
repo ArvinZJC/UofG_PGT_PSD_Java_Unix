6c4c9fd56e353c336c37c65cb3f3937aaedb6b18
ArvinZJC <tomzjc@qq.com>
2021-02-03 12:10:34 +0000

    Complete Lab 6.

---
+++ Lab/6/T1/T1.java
@@ -0,0 +1,38 @@
+/*
+ * @Description: Task 1
+ * @Version: 1.0.0.20210202
+ * @Author: Arvin Zhao
+ * @Date: 2021-02-02 22:23:10
+ * @Last Editors: Arvin Zhao
+ * @LastEditTime: 2021-02-02 22:56:05
+ */
+
+class T1
+{
+    /**
+     * Compute and print the list of prime numbers less than a given paramter.
+     * @param max a given parameter
+     */
+    static void printPrimes(int max)
+    {
+        for (int n = 2; n < max; n++)
+        {
+            boolean isPrime = true;
+
+            for (int f = 2; f <= Math.sqrt(n); f++)
+                if (n % f == 0)
+                {
+                    isPrime = false;
+                    break;
+                } // end if
+
+            if (isPrime)
+                System.out.println(n + " is prime.");
+        } // end for
+    } // end method printPrimes
+
+    public static void main(String[] args)
+    {
+        printPrimes(20);
+    } // end main
+} // end class T1
---
+++ Lab/6/T2/T2.java
@@ -0,0 +1,36 @@
+/*
+ * @Description: Task 2
+ * @Version: 1.0.0.20210202
+ * @Author: Arvin Zhao
+ * @Date: 2021-02-02 22:36:35
+ * @Last Editors: Arvin Zhao
+ * @LastEditTime: 2021-02-02 22:44:06
+ */
+
+class T2
+{
+    /**
+     * Compute and return the n-th Fibonacci number.
+     * @param n number n in a series
+     * @return the n-th Fibonacci number
+     */
+    static int computeFibonacci(int n)
+    {
+        int result = 1;
+        int lastResult = 1;
+
+        for (int i = 2; i < n; i++)
+        {
+            int temp = result;
+            result += lastResult;
+            lastResult = temp;
+        } // end for
+
+        return result;
+    } // end method computeFibonacci
+
+    public static void main(String[] args)
+    {
+        System.out.println(computeFibonacci(10));
+    } // end main
+} // end class T2
